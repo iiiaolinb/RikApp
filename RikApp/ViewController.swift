c2cbbc288465ac13b69e2afbc2b7ea5ac321574e
Khudyaev Egor <ekhudiaev@emphasoft.com>
2025-11-24 20:54:46 +0300

    initial commit

---
+++ RikApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RikApp
+//
+//  Created by Егор Худяев on 24.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
